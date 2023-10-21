31c44b59fd87391ac76495db055ddc0f93de9ab3
ras0q <ras0q@users.noreply.github.com>
2023-10-21 16:24:57 +0900

    C2T1S1: use swift-dependencies (error remains)

--- Sources/RepositoryListFeature/RepositoryListView.swift
+++ Sources/RepositoryListFeature/RepositoryListView.swift
@@ -106,34 +106,11 @@ public struct RepositoryList: Reducer {
             await send(
                 .searchRepositoriesResponse(
                     TaskResult {
-                        let url = URL(
-                                      string: "https://api.github.com/search/repositories?q=\(query)&sort=stars"
-                                    )!
-                        var request = URLRequest(url: url)
-                        if let token = Bundle.main.infoDictionary?["GitHubPersonalAccessToken"] as? String {
-                          request.setValue(
-                            "Bearer \(token)",
-                            forHTTPHeaderField: "Authorization"
-                          )
-                        }
-                        let (data, _) = try await URLSession.shared.data(for: request)
-                        let repositories = try jsonDecoder.decode(
-                          GithubSearchResult.self,
-                          from: data
-                        ).items
-                        return repositories
                     }
                 )
             )
         }
     }
-
-
-    private let jsonDecoder: JSONDecoder = {
-      let decoder = JSONDecoder()
-      decoder.keyDecodingStrategy = .convertFromSnakeCase
-      return decoder
-    }()
 }
 
 public struct RepositoryListView: View {
